dc2d90359fc742f3e8a7ed613844c089c19fcb87
urano_s <urano3104@gmail.com>
2021-04-13 21:23:25 +0900

    add first button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <Invitation /> -->
+  <MoveIconButton />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Invitation from './components/Invitation.vue'
+import MoveIconButton from './components/MoveIconButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Invitation,
+    MoveIconButton
   }
 }
 </script>
 
 <style>
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
 #app {
+  height: 100vh;
+  margin: 2em;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
 }
+
 </style>
---
+++ src/components/Invitation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="invitation">
+    this is invitation
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.invitation {
+  margin: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
